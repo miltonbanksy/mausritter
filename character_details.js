d66e4f27e4672b32488c7e44955d6b68299f1ebd
miltonbanksy <miltonbanksy@gmail.com>
2026-01-13 14:15:27 +0900

    Add initial game setup files including backgrounds, character details, names, and HTML structure

---
+++ character_details.js
@@ -0,0 +1,65 @@
+birthsigns = [
+  "Star - Brave / Reckless",
+  "Wheel - Industrious / Unimaginative",
+  "Acorn - Inquisitive / Stubborn",
+  "Storm - Generous / Wrathful",
+  "Moon - Wise / Mysterious",
+  "Mother - Nurturing / Worrying"
+];
+
+coat_colors = [
+  "Chocolate",
+  "Black",
+  "White",
+  "Tan",
+  "Gray",
+  "Blue"
+];
+
+coat_patterns = [
+  "Solid",
+  "Brindle",
+  "Patchy",
+  "Banded",
+  "Marbled",
+  "Flecked"
+];
+
+const features = [
+  "Scarred body",
+  "Groomed fur",
+  "Corpulent body",
+  "Dreadlocks",
+  "Skeletal body",
+  "Dyed fur",
+  "Willowy body",
+  "Shaved fur",
+  "Tiny body",
+  "Frizzy fur",
+  "Massive body",
+  "Silky fur",
+  "War paint",
+  "Night black eyes",
+  "Foreign clothes",
+  "Eye patch",
+  "Elegant clothes",
+  "Blood red eyes",
+  "Patched clothes",
+  "Wise eyes",
+  "Fashionable clothes",
+  "Sharp eyes",
+  "Unwashed clothes",
+  "Luminous eyes",
+  "Missing ear",
+  "Cropped tail",
+  "Lumpy face",
+  "Whip-like tail",
+  "Beautiful face",
+  "Tufted tail",
+  "Round face",
+  "Stubby tail",
+  "Delicate face",
+  "Prehensile tail",
+  "Elongated face",
+  "Curly tail"
+];
